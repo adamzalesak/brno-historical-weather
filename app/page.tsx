174fa3cc7931eed550141e299647ed2c45eb67ae
Adam Zálešák <adam.zalesak@notino.com>
2023-11-27 11:03:28 +0100

    feat: add Shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import AuthButton from "../components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
@@ -23,6 +24,9 @@ export default async function Index() {
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
           {isSupabaseConnected && <AuthButton />}
+          <Button variant="destructive" size="lg">
+            Click me
+          </Button>
         </div>
       </nav>
 

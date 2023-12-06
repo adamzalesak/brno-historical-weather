e91b21d8d4dd13e2984342c145a1574c89ad4860
mikulas <heinz.mikulas@gmail.com>
2023-12-06 21:00:41 +0100

    homepage datepicker

--- components/DateSelector.tsx
+++ components/DateSelector.tsx
@@ -8,19 +8,27 @@ import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { DatePicker } from "@/components/ui/date-picker";
 import { DatePickerWithRange } from "@/components/ui/date-range-picker";
 
+const DatePickersDivider = () => {
+  return (
+    <>
+      <div className="flex items-center my-4">
+        <div className="flex-grow border-t border-gray-300"></div>
+        <span className="mx-2 text-sm text-gray-600">OR</span>
+        <div className="flex-grow border-t border-gray-300"></div>
+      </div>
+      <div className="text-xs text-gray-500 text-center mb-4">
+        select time period you are interested in
+      </div>
+    </>
+  );
+};
+
 const DateSelector = () => {
   return (
     <Card>
       <CardContent>
-        <DatePicker />
-        <div className="flex items-center my-4">
-          <div className="flex-grow border-t border-gray-300"></div>
-          <span className="mx-2 text-sm text-gray-600">OR</span>
-          <div className="flex-grow border-t border-gray-300"></div>
-        </div>
-        <div className="text-xs text-gray-500 text-center mb-4">
-          select time period you are interested in
-        </div>
+        <DatePicker className={"pt-8"} />
+        <DatePickersDivider />
         <DatePickerWithRange />
       </CardContent>
       <CardFooter>

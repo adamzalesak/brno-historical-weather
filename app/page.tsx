60766734ad05826784987852359ff50213c9d457
mikulas <heinz.mikulas@gmail.com>
2023-12-06 21:10:06 +0100

    homepage remove footer

--- app/page.tsx
+++ app/page.tsx
@@ -27,15 +27,13 @@ export default async function Index() {
   const isSupabaseConnected = canInitSupabaseClient();
 
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
+    <div className="flex-1 w-full flex flex-col gap-20 items-center h-screen">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
           {isSupabaseConnected && <AuthButton />}
           <Header />
         </div>
       </nav>
-
-      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3"></div>
 
       <Card>
         <CardHeader>
@@ -46,10 +44,7 @@ export default async function Index() {
           </CardDescription>
         </CardHeader>
       </Card>
-
       <DateSelector />
-
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs"></footer>
     </div>
   );
 }

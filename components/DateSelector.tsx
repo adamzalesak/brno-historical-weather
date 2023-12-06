62946e8935b44efe4570a9b50c581b5abe0ec7e5
mikulas <heinz.mikulas@gmail.com>
2023-12-06 20:51:19 +0100

    prettier format

--- components/DateSelector.tsx
+++ components/DateSelector.tsx
@@ -1,35 +1,34 @@
-'use client'
+"use client";
 
-import * as React from "react"
+import * as React from "react";
 
+import { Button } from "@/components/ui/button";
 
-import { Button } from "@/components/ui/button"
-
-import {Card, CardContent, CardFooter} from "@/components/ui/card";
-import {DatePicker} from "@/components/ui/date-picker";
-import {DatePickerWithRange} from "@/components/ui/date-range-picker";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { DatePicker } from "@/components/ui/date-picker";
+import { DatePickerWithRange } from "@/components/ui/date-range-picker";
 
 const DateSelector = () => {
-    return (
-
-        <Card>
-            <CardContent>
-                <DatePicker/>
-                <div className="flex items-center my-4">
-                    <div className="flex-grow border-t border-gray-300"></div>
-                    <span className="mx-2 text-sm text-gray-600">OR</span>
-                    <div className="flex-grow border-t border-gray-300"></div>
-                </div>
-                <div className="text-xs text-gray-500 text-center mb-4">
-                    select time period you are interested in
-                </div>
-                    <DatePickerWithRange/>
-            </CardContent>
-            <CardFooter>
-                <Button className={'w-full'} variant={'default'}>Explore</Button>
-            </CardFooter>
-
-        </Card>
-    )
-}
-export default DateSelector
+  return (
+    <Card>
+      <CardContent>
+        <DatePicker />
+        <div className="flex items-center my-4">
+          <div className="flex-grow border-t border-gray-300"></div>
+          <span className="mx-2 text-sm text-gray-600">OR</span>
+          <div className="flex-grow border-t border-gray-300"></div>
+        </div>
+        <div className="text-xs text-gray-500 text-center mb-4">
+          select time period you are interested in
+        </div>
+        <DatePickerWithRange />
+      </CardContent>
+      <CardFooter>
+        <Button className={"w-full"} variant={"default"}>
+          Explore
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
+export default DateSelector;
--- components/ui/date-picker.tsx
+++ components/ui/date-picker.tsx
@@ -1,47 +1,47 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { format } from "date-fns"
-import { Calendar as CalendarIcon } from "lucide-react"
+import * as React from "react";
+import { format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Calendar } from "@/components/ui/calendar"
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
-} from "@/components/ui/popover"
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
 export function DatePicker({
-                               className,
-                           }: React.HTMLAttributes<HTMLDivElement>) {
-    const [date, setDate] = React.useState<Date>()
+  className,
+}: React.HTMLAttributes<HTMLDivElement>) {
+  const [date, setDate] = React.useState<Date>();
 
-    return (
-        <div className={cn("grid gap-2", className)}>
-        <Popover>
-            <PopoverTrigger asChild>
-                <Button
-                    variant={"outline"}
-                    className={cn(
-                        "w-[280px] justify-start text-left font-normal",
-                        !date && "text-muted-foreground"
-                    )}
-                >
-                    <CalendarIcon className="mr-2 h-4 w-4" />
-                    {date ? format(date, "PPP") : <span>Pick a date</span>}
-                </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-auto p-0">
-                <Calendar
-                    mode="single"
-                    selected={date}
-                    onSelect={setDate}
-                    initialFocus
-                />
-            </PopoverContent>
-        </Popover>
-        </div>
-    )
+  return (
+    <div className={cn("grid gap-2", className)}>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            variant={"outline"}
+            className={cn(
+              "w-[280px] justify-start text-left font-normal",
+              !date && "text-muted-foreground",
+            )}
+          >
+            <CalendarIcon className="mr-2 h-4 w-4" />
+            {date ? format(date, "PPP") : <span>Pick a date</span>}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0">
+          <Calendar
+            mode="single"
+            selected={date}
+            onSelect={setDate}
+            initialFocus
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
 }

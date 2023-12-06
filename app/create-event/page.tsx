7281519f56aa676d91522218f0fe2722584006ad
mikulas <heinz.mikulas@gmail.com>
2023-12-06 22:03:07 +0100

    create event wip

---
+++ app/create-event/page.tsx
@@ -0,0 +1,20 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+const CreateEvent = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Create my event</CardTitle>
+        <CardDescription>Edit your event's attributes</CardDescription>
+        <CardContent></CardContent>
+      </CardHeader>
+    </Card>
+  );
+};
+export default CreateEvent;
